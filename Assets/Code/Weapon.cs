c94c7fff1c4429bcbe800dea69c8793ed1f24e72
TheChunkierBean <thechunkierbean@gmail.com>
2016-05-14 00:13:44 -0400

    First Person Animations Work! Could use some tweaks
    Also new HUD Stuff

--- Assets/Code/Weapon.cs
+++ Assets/Code/Weapon.cs
@@ -8,11 +8,14 @@ public class Weapon: MonoBehaviour {
     public float fireRate = 0.3f;
     public float nextFire;
     RaycastHit hit;
+    private Animator anim;
     //public GameObject flare;
 
 
 	// Use this for initialization
 	void Start () {
+
+        anim = GetComponent<Animator>();
 	
 	}
 	
@@ -21,36 +24,57 @@ public class Weapon: MonoBehaviour {
 
         //flare.SetActive(false);
         Fire();
-        //Reload();
+        Reload();
         Melee();
+        Throw();
 	}
+
 
     void Fire()
     {
         if (Input.GetKey(KeyCode.Mouse0) && Time.time > nextFire)
         {
+            if (anim != null)
+            {
+                anim.SetTrigger("Fire");
+                nextFire = Time.time + fireRate;
+                Debug.Log("Fire!");
+            }
 
-            //.SetActive(true);
-            nextFire = Time.time + fireRate;
-            GetComponent<Animation>().Play("Fire1");
-            Debug.Log("Fire!");
         }
     }
 
-    /*void Reload()
+    void Reload()
     {
         if (Input.GetKey(KeyCode.R))
         {
-            GetComponent<Animation>().Play("Reload");
+            if (anim != null)
+            {
+                anim.SetTrigger("Reload");
+            }
         }
     }
-     */
+
 
     void Melee()
     {
         if (Input.GetKey(KeyCode.F))
         {
-            GetComponent<Animation>().Play("Melee");
+            if (anim != null)
+            {
+                anim.SetTrigger("Melee");
+            }
+        }
+    }
+
+    void Throw()
+    {
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            if (anim != null)
+            {
+                anim.SetTrigger("Throw");
+            }
         }
     }
 
